22847e44de2bb12a4cd31bbedcd7f0c942635af3
warddaraghmeh <warddaraghmeh99@gmail.com>
2023-11-08 16:17:51 +0200

    Solution the first Task

--- lib/main.dart
+++ lib/main.dart
@@ -11,53 +11,154 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
-      home: SimpleProject(),
+    return const MaterialApp(
+      home: FacebookApp(),
     );
   }
 }
 
-class SimpleProject extends StatelessWidget {
-  const SimpleProject({super.key});
+class FacebookApp extends StatelessWidget {
+  const FacebookApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 10,
-        title: Text("Facebook"),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.menu,
-            size: 33,
+        appBar: AppBar(
+          title: Text("facebook",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 27,
+                  fontWeight: FontWeight.w700)),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.blueAccent,
+              size: 30,
+            ),
+            onPressed: () {},
           ),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.message,
+                  color: Colors.blueAccent,
+                  size: 30,
+                ),
+                onPressed: () {}),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.blueAccent,
+                size: 30,
+              ),
+              onPressed: () {},
+            ),
+          ],
+          backgroundColor: Colors.white,
+          elevation: 20,
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.message,
-              size: 33,
-            ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 22),
+                alignment: Alignment.center,
+                // ignore: sort_child_properties_last
+                child: Text(
+                  "C4a.shop",
+                  style: TextStyle(color: Colors.white, fontSize: 44),
+                ),
+                height: 322,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blueGrey,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      // ignore: sort_child_properties_last
+                      child: Text(
+                        "ward",
+                        style: TextStyle(color: Colors.white, fontSize: 27),
+                      ),
+                      height: 100,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 23),
+                      alignment: Alignment.center,
+                      // ignore: sort_child_properties_last
+                      child: Text(
+                        "Hakam",
+                        style: TextStyle(color: Colors.white, fontSize: 27),
+                      ),
+                      height: 100,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 23),
+                      alignment: Alignment.center,
+                      // ignore: sort_child_properties_last
+                      child: Text(
+                        "Hakam",
+                        style: TextStyle(color: Colors.white, fontSize: 27),
+                      ),
+                      height: 100,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 23),
+                      alignment: Alignment.center,
+                      // ignore: sort_child_properties_last
+                      child: Text(
+                        "Hakam",
+                        style: TextStyle(color: Colors.white, fontSize: 27),
+                      ),
+                      height: 100,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 22),
+                alignment: Alignment.center,
+                // ignore: sort_child_properties_last
+                child: Text(
+                  "C4a.shop",
+                  style: TextStyle(color: Colors.white, fontSize: 44),
+                ),
+                height: 322,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ],
           ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.search,
-              size: 33,
-            ),
-          ),
-        ],
-      ),
-      body: Center(
-          child: Text(
-        "Hello,ward 👋",
-        style: TextStyle(fontSize: 40),
-      )),
-    );
+        ));
   }
 }
