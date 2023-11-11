30fbe91ce2ab4f8dc5101ad0e4160c6ede3e8c19
warddaraghmeh <warddaraghmeh99@gmail.com>
2023-11-11 13:38:30 +0200

    flutter Expanded and flex

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: FacebookApp(),
     );
   }
@@ -25,116 +24,72 @@ class FacebookApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("facebook",
-            style: TextStyle(
-                color: Colors.blueAccent,
-                fontSize: 27,
-                fontWeight: FontWeight.w700)),
+        title: Text("facebook", style: TextStyle(color: Colors.blueAccent, fontSize: 30, fontWeight:  FontWeight.w700)),
         centerTitle: true,
         leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.blueAccent,
-            size: 30,
-          ),
+          icon: Icon(Icons.menu, color: Colors.blueAccent, size: 30, ),
           onPressed: () {},
         ),
         actions: [
           IconButton(
-              icon: Icon(
-                Icons.message,
-                color: Colors.blueAccent,
-                size: 30,
-              ),
-              onPressed: () {}),
+            icon: Icon(Icons.message, color: Colors.blueAccent, size: 30,), 
+            onPressed: () {}),
           IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.blueAccent,
-              size: 30,
-            ),
+            icon: Icon(Icons.search, color: Colors.blueAccent, size: 30,),
             onPressed: () {},
           ),
         ],
+        
         backgroundColor: Colors.white,
         elevation: 20,
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(9),
-          child: Stack(
-            children: [
-              Positioned(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "c4a.shop",
-                    style: TextStyle(fontSize: 22, color: Colors.white),
-                  ),
-                  color: Colors.pink[200],
-                ),
-                bottom: 0,
-                left: 0,
+    
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.center,
+                child: Text("c4a.shop", style: TextStyle(fontSize: 19, ),),
+                color: Colors.amber[200],
+                width: 99,
                 height: 100,
               ),
-              Positioned(
-                height: 100,
-                width: 100,
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "c4a.shop",
-                    style: TextStyle(fontSize: 22),
-                  ),
-                  color: Colors.green[200],
-                ),
-              ),
-              Positioned(
-                right: 0,
-                top: 0,
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "c4a.shop",
-                    style: TextStyle(fontSize: 22),
-                  ),
-                  width: 100,
-                  height: 100,
-                  color: Colors.amber[200],
-                ),
-              ),
-              Positioned(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "c4a.shop",
-                    style: TextStyle(fontSize: 22, color: Colors.white),
-                  ),
-                  color: Colors.blue[300],
-                ),
-                bottom: 0,
-                right: 0,
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.center,
+                child: Text("c4a.shop", style: TextStyle(fontSize: 19, color: Colors.white),),
+                color: Colors.blue[300],
+                width: 99,
                 height: 100,
               ),
-              Center(
-                child: Container(
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
                   alignment: Alignment.center,
-                  child: Text(
-                    "c4a.shop",
-                    style: TextStyle(fontSize: 22, color: Colors.white),
-                  ),
-                  width: 100,
-                  height: 100,
+                  child: Text("c4a.shop", style: TextStyle(fontSize: 19, color: Colors.white),),
                   color: Colors.red[300],
-                ),
-              ),
-            ],
-          ),
-          width: 300,
-          height: 300,
-          color: Colors.blueGrey,
+                  width: 99,
+                  height: 100,
+            ),
+              
+              
+              )
+          
+          
+          
+          ],
         ),
+        color: Colors.blueGrey,
+        height: 500,
+        width: double.infinity,
       ),
+    
+    
     );
   }
 }
